482d891cbf5d397c683a7c61aa6d10f3a4ad7961
pez <perr@arch.com>
2024-10-10 06:39:00 +0100

    Decoding.py now accepts commandline arguments.

--- decoding.py
+++ decoding.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 import base64
+import sys
+
+def get_args():
+    if len(sys.argv) != 3:
+        raise Exception("Usage: [Input File] [Output File]")
+    else:
+        input = sys.argv[1]
+        output = sys.argv[2]
+        return (input, output)
 
 def main():
-    input = open("/home/haxor/Code/Personal/C++/CryptoPals/Set1/src/Texts/challenge6.txt", 'rb')
-    output = open("challenge6_decoded.txt", 'wb')
-    base64.decode(input, output)
+    input_file, output_file = get_args()
+    OUTPUT_DIR = "./Texts/"
+    try:
+        input= open(OUTPUT_DIR + input_file, 'rb')
+        output = open(OUTPUT_DIR + output_file, 'wb')
+
+        base64.decode(input, output)
+    except Exception as e:
+        print(e)
     return 0
 
 if __name__ == "__main__":
