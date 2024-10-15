a53266f785e1abb89a6d9c42b033b72e467df204
pez <perr@arch.com>
2024-10-15 12:58:00 +0100

    File Lines Read :)

--- challenge8.cpp
+++ challenge8.cpp
@@ -1,6 +1,15 @@
 #include "utility.hpp"
 
 int main(void) {
-    std::cout << "Hello World!" << std::endl;
+    const std::string filename = "./Texts/challenge8.txt";
+    std::map<int, std::vector<uint8_t>> file_lines = read_lines(filename);
+    auto iter = file_lines.begin();
+    while (iter != file_lines.end()) {
+        std::cout << iter->first << ") ";
+        print_array(iter->second);
+        std::cout << "\n\n";
+        iter++;
+    }
+
     return 0;
 }
