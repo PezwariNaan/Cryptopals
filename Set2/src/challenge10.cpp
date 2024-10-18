464d8ba700297ca8a4fa481c994a8d8e41c507c7
pez <perr@arch.com>
2024-10-18 16:50:53 +0100

    Challenge10 outlined and decoded file read.

--- Set2/src/challenge10.cpp
+++ Set2/src/challenge10.cpp
@@ -1,6 +1,25 @@
 #include "utility.hpp"
 
+// Generate IV
+std::vector<uint8_t> generate_iv(void) {
+    std::vector<uint8_t> iv;
+
+    for (char i = 0; i < 16; i++) {
+        iv.push_back(0);
+    }
+
+    return iv;
+} 
+// Encrypt Block 
+
+// XOR Block 
+
+// Implement CBC
+
 int main(void) {
-    std::cout << "Hello World!" << std::endl;
+
+    std::vector<uint8_t> read_file =  read_file_bytes("./Texts/challenge10_decoded");
+    print_array(read_file);
+    std::cout << std::endl;
     return 0;
 }
--- decoding.py
+++ decoding.py
@@ -4,19 +4,20 @@ import base64
 import sys
 
 def get_args():
-    if len(sys.argv) != 3:
-        raise Exception("Usage: [Input File] [Output File]")
+    if len(sys.argv) != 4:
+        raise Exception("Usage: [Set] [Input File] [Output File]")
     else:
-        input = sys.argv[1]
-        output = sys.argv[2]
-        return (input, output)
+        cp_set = sys.argv[1]
+        input = sys.argv[2]
+        output = sys.argv[3]
+        return (cp_set, input, output)
 
 def main():
-    input_file, output_file = get_args()
-    OUTPUT_DIR = "./Texts/"
+    cp_set, input_file, output_file = get_args()
+    FILE_DIR = f"{cp_set}/src/Texts/"
     try:
-        input= open(OUTPUT_DIR + input_file, 'rb')
-        output = open(OUTPUT_DIR + output_file, 'wb')
+        input= open(FILE_DIR + input_file, 'rb')
+        output = open(FILE_DIR + output_file, 'wb')
 
         base64.decode(input, output)
     except Exception as e:
