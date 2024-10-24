f16431a82401bb023f04a73e8b17c4b722d79bff
pez <perr@arch.com>
2024-10-24 11:08:32 +0100

    ECB Encryption function implemented :).

--- Set2/src/challenge11.cpp
+++ Set2/src/challenge11.cpp
@@ -1,5 +1,8 @@
 #include "utility.hpp"
 #include <chrono>
+#include <cstdint>
+#include <openssl/evp.h>
+#include <stdexcept>
 #include <thread>
 
 // Generate Key
@@ -21,36 +24,39 @@ std::vector<uint8_t> padd_ciphertext(std::vector<uint8_t> ciphertext) {
 }
 
 // Encrypt Function With CBC
-std::vector<uint8_t> encrypt_cbc(std::vector<uint8_t> key, std::vector<uint8_t> plaintext){
+std::vector<uint8_t> encrypt_cbc(EVP_CIPHER_CTX *ctx, std::vector<uint8_t> key, std::vector<uint8_t> plaintext){
     std::vector<uint8_t> ciphertext;
+
     return ciphertext;
 }
 
 // Encrypt Fucntion With ECB
-std::vector<uint8_t> encrypt_ecb(std::vector<uint8_t> key, std::vector<uint8_t> plaintext) {
-    std::vector<uint8_t> ciphertext;
+std::vector<uint8_t> encrypt_ecb(EVP_CIPHER_CTX *ctx, std::vector<uint8_t> plaintext) {
+    if (EVP_EncryptInit(ctx, EVP_aes_128_ecb(), generate_key().data(), NULL) != 1)
+        throw std::runtime_error("Error Initalising Encryption Engine.");
+
+    std::vector<uint8_t> ciphertext(plaintext.size() + EVP_CIPHER_CTX_block_size(ctx));
+    int ciphertext_len = 0;
+    int len = 0;
+
+    if (EVP_EncryptUpdate(ctx, ciphertext.data(), &len, plaintext.data(), plaintext.size()) != 1)
+        throw std::runtime_error("Error With ECB Encryption."); 
+    ciphertext_len = len;
+
+    if (EVP_EncryptFinal(ctx, ciphertext.data() + ciphertext_len, &len) != 1)
+        throw std::runtime_error("Error With ECB Encryption (Final Step).");
+    ciphertext_len += len;
+    
+    ciphertext.resize(ciphertext_len);
+    
     return ciphertext;
 }
 
 int main(void) {
-    std::vector<uint8_t> key1 = generate_key();
-    print_array(key1);
-    std::cout << "\n";
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::vector<uint8_t> key2 = generate_key();
-    print_array(key2);
-    std::cout << "\n";
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::vector<uint8_t> key3 = generate_key();
-    print_array(key3);
-    std::cout << "\n";
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::vector<uint8_t> key4 = generate_key();
-    print_array(key4);
-    std::cout << "\n";
+    EVP_CIPHER_CTX *ctx =  EVP_CIPHER_CTX_new();
+    std::string plaintext_string = "Hello Darkness My Old Friend";
+    std::vector<uint8_t> plaintext(plaintext_string.begin(), plaintext_string.end());
+    
 
     return 0;
 }
