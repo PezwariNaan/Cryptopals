b68a3526fed4434ed1fd5bcb43a514c6a3d837e5
pez <perr@arch.com>
2024-11-11 14:13:21 +0000

    Blocksize deduced.

--- Set2/src/challenge12.cpp
+++ Set2/src/challenge12.cpp
@@ -1,32 +1,51 @@
+#include "openssl.hpp"
 #include "utility.hpp"
 #include "encoding.hpp"
+// --------------------
+#include <cstdint>
 #include <openssl/evp.h>
-
-std::vector<uint8_t> encrypt_ecb(EVP_CIPHER_CTX *ctx, const std::vector<uint8_t> plaintext, const std::vector<uint8_t> key) {
-    if (EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key.data(), NULL) != 1)
-        throw std::runtime_error("Error Initalising Encryption Engine.");
-
-    std::vector<uint8_t> ciphertext(plaintext.size() + EVP_CIPHER_CTX_block_size(ctx));
-    int ciphertext_len = 0;
-    int len = 0;
-
-    if (EVP_EncryptUpdate(ctx, ciphertext.data(), &len, plaintext.data(), plaintext.size()) != 1)
-        throw std::runtime_error("Error With ECB Encryption."); 
-    ciphertext_len = len;
-
-    if (EVP_EncryptFinal(ctx, ciphertext.data() + ciphertext_len, &len) != 1)
-        throw std::runtime_error("Error With ECB Encryption (Final Step).");
-    ciphertext_len += len;
-    
-    ciphertext.resize(ciphertext_len);
-    
-    return ciphertext;
-}
+#include <sys/types.h>
+#include <vector>
 
 int main(int argc, char *argv[]) {
     std::string filename = "Texts/challenge12.txt";
+    std::vector<uint8_t> plaintext = cp::py_base64decode(filename, *argv);
+    std::vector<uint8_t> padded_plaintext = plaintext; // Variable that plaintext will be copied into before padding
+    std::vector<uint8_t> key = {'A'};
+    std::vector<uint8_t> padding;
+    size_t blocksize = 0;
+    size_t unpadded_ciphertext_size = 0;
 
-    std::vector<uint8_t> decoded = cp::py_base64decode(filename, *argv);
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+    std::vector<uint8_t> ciphertext = openssl::encrypt_ecb(ctx, plaintext, key); // What we are attempting to decrypt
     
+    // TODO:
+
+    //      Pad & Encrypt plaintext 
+    std::vector<uint8_t> padded_ciphertext = openssl::encrypt_ecb(ctx, padded_plaintext, key);
+
+    int padding_size = 0;
+    while (ciphertext.size() == padded_ciphertext.size()) {
+        padding_size++;
+        padding.assign(padding_size, 'B');
+        padded_plaintext = plaintext;
+        padded_plaintext.insert(padded_plaintext.begin(), padding.begin(), padding.end());
+        padded_ciphertext = openssl::encrypt_ecb(ctx, padded_plaintext, key);
+    }
+
+    blocksize = padded_ciphertext.size() - ciphertext.size();
+    unpadded_ciphertext_size = ciphertext.size() - (blocksize - padding_size);
+    int existing_padding = blocksize - padding_size - 1;
+
+    //      Detect ECB mode
+    //      Detect block size
+    //      Detect unpadded ciphertext size (ciphertext.size() - (padding.size() + blocksize))
+    //      Pad ciphertext blocksize - 1
+    //      Test final byte == ciphertext guess 
+    //      Repeat for next byte 
+    //      Print result
+
+
     return 0;
 }
