2cc38fdeec932423f9e85a50b29c7d686d513d02
pez <perr@arch.com>
2024-11-11 11:57:59 +0000

    ECB Encrypt function added to challenge12.cpp.

--- Set2/src/challenge12.cpp
+++ Set2/src/challenge12.cpp
@@ -1,11 +1,32 @@
 #include "utility.hpp"
 #include "encoding.hpp"
+#include <openssl/evp.h>
+
+std::vector<uint8_t> encrypt_ecb(EVP_CIPHER_CTX *ctx, const std::vector<uint8_t> plaintext, const std::vector<uint8_t> key) {
+    if (EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key.data(), NULL) != 1)
+        throw std::runtime_error("Error Initalising Encryption Engine.");
+
+    std::vector<uint8_t> ciphertext(plaintext.size() + EVP_CIPHER_CTX_block_size(ctx));
+    int ciphertext_len = 0;
+    int len = 0;
+
+    if (EVP_EncryptUpdate(ctx, ciphertext.data(), &len, plaintext.data(), plaintext.size()) != 1)
+        throw std::runtime_error("Error With ECB Encryption."); 
+    ciphertext_len = len;
+
+    if (EVP_EncryptFinal(ctx, ciphertext.data() + ciphertext_len, &len) != 1)
+        throw std::runtime_error("Error With ECB Encryption (Final Step).");
+    ciphertext_len += len;
+    
+    ciphertext.resize(ciphertext_len);
+    
+    return ciphertext;
+}
 
 int main(int argc, char *argv[]) {
-    std::string filename = "Texts/challenge10.txt";
+    std::string filename = "Texts/challenge12.txt";
 
     std::vector<uint8_t> decoded = cp::py_base64decode(filename, *argv);
-    std::cout.write(reinterpret_cast<char *>(decoded.data()), decoded.size());
-    std::cout << std::endl;
+    
     return 0;
 }
