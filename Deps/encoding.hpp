b4fe4651d210237f872f67659159b3c3346de967
pez <perr@arch.com>
2025-01-08 05:10:55 +0000

    Minor Cleanup

--- Deps/encoding.hpp
+++ Deps/encoding.hpp
@@ -9,12 +9,11 @@
 #include <cmath>
 
 
-// Base64 Functions
 namespace cp {
+    // Base64 Functions
     std::vector<uint8_t> base64_decode(const std::vector<uint8_t> &input);
     std::string base64_encode(const std::vector<uint8_t> &input);
     std::vector<uint8_t> py_base64decode(const std::string filename, const char *argv);
-
 
     // Hex Functions
     std::vector<uint8_t> hex_decode(const std::vector<uint8_t> &input);
