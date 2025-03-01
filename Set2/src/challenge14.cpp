7bc2631994fe72d08161538754ce770ed4bcc746
pez <perr@arch.com>
2025-03-01 19:23:48 +0000

    Praise the Python God's, b64 decode in C++ <3

--- Set2/src/challenge14.cpp
+++ Set2/src/challenge14.cpp
@@ -1,6 +1,7 @@
 #include "utility.hpp"
 
 #define BYTES std::vector<uint8_t>
+#define BASE64PAD '='
 
 class DecodeError : public std::exception {
     private: 
@@ -13,6 +14,27 @@ class DecodeError : public std::exception {
         }
 };
 
+constexpr uint8_t b64de_table[256] = {
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255, 62, 255,255,255, 63,
+    52 , 53, 54, 55,  56, 57, 58, 59,  60, 61,255,255, 255,  0,255,255,
+
+    255,  0,  1,  2,   3,  4,  5,  6,   7,  8,  9, 10,  11, 12, 13, 14,
+    15 , 16, 17, 18,  19, 20, 21, 22,  23, 24, 25,255, 255,255,255,255,
+    255, 26, 27, 28,  29, 30, 31, 32,  33, 34, 35, 36,  37, 38, 39, 40,
+    41 , 42, 43, 44,  45, 46, 47, 48,  49, 50, 51,255, 255,255,255,255,
+
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255};
+
 int b64_to_char(char c) {
     if (c >= 'A' && c <= 'Z') return c - 'A';
     if (c >= 'a' && c <= 'z') return c - 'a' + 26;
@@ -22,41 +44,129 @@ int b64_to_char(char c) {
     return -1;
 }
 
-BYTES b64_decode(const std::string loaded_file) {
-    BYTES vec_loaded_file(loaded_file.begin(), loaded_file.end());
+uint8_t* b64_decode(const std::string buffer, size_t &length, bool strict_mode) {
+      std::string error_message;
 
-    BYTES vec_decoded;
+    const uint8_t *ascii_data = (const uint8_t *)buffer.data();
+    size_t ascii_len = length;
+    bool padding_started = 0;
 
-    // Get first 4 characters (24 bits)
-    // Convert base64 value to 6 bit value
-    for (size_t i = 0; i < vec_loaded_file.size(); i++) {
-        vec_loaded_file[i] =  b64_to_char(vec_loaded_file[i]);
+    size_t bin_len = ascii_len / 4 * 3; 
+    uint8_t *bin_data = new (std::nothrow) uint8_t[bin_len + 1];
+    if(!bin_data) {
+        throw std::runtime_error("Failed to allocate memory for bin_data.");
+    }
+    uint8_t *bin_data_start = bin_data;
+    bin_data[bin_len] = 0x0;
+
+    uint8_t leftchar = 0;
+    uint32_t quad_pos = 0;
+    uint32_t pads = 0;
+
+    if(strict_mode && (ascii_len > 0) && (*ascii_data == BASE64PAD)) {
+        error_message = "Leading padding not allowed.";
+        goto error_end;
     }
 
-    // Go 4 bytes at a time & Concat them into one variable
-    for (size_t i = 0; i < vec_loaded_file.size() ; i += 4) {
-        BYTES block(vec_loaded_file.begin() + i, vec_loaded_file.begin() + (i + 4));
-        uint32_t combined = (block[0] << 18 | block[1] << 12 | block[2] << 6 | block[3]);
+    size_t i;
+    uint8_t this_ch;
+    for(i = 0; i < ascii_len; ++i) {
+        this_ch = ascii_data[i];
 
-        uint8_t first_byte  = combined >> 16;
-        uint8_t second_byte = combined >> 8;
-        uint8_t third_byte  = combined;
-        
-        vec_decoded.push_back(first_byte);
-        vec_decoded.push_back(second_byte);
-        vec_decoded.push_back(third_byte);
+        if(this_ch == BASE64PAD) {
+            padding_started = true;
+            // If the current character is a padding character, the length
+            // will be reduced by one to obtain the decoded true length.
+            bin_len--;
+
+            if(strict_mode && (!quad_pos)) {
+                error_message = "Excess padding not allowed.";
+                goto error_end;
+            }
+
+            if((quad_pos >= 2) && (quad_pos + (++pads) >= 4)) {
+
+                if(strict_mode && ((i + 1) < ascii_len)) {
+                    error_message = "Excess data after padding.";
+                    goto error_end;
+                }
+
+                goto done;
+            }
+
+            continue;
+        }
+
+        this_ch = b64de_table[this_ch];
+        if(this_ch == 255) {
+            if(strict_mode) {
+                error_message = "Only base64 data is allowed.";
+                goto error_end;
+            }
+            continue;
+        }
+
+        if(strict_mode && padding_started) {
+            error_message = "Discontinuous padding not allowed.";
+            goto error_end;
+        }
+
+        pads = 0;
+
+        switch(quad_pos) {
+        case 0:
+            quad_pos = 1;
+            leftchar = this_ch;
+            break;
+        case 1:
+            quad_pos = 2;
+            *bin_data++ = (leftchar << 2) | (this_ch >> 4);
+            leftchar = this_ch & 0xf;
+            break;
+        case 2:
+            quad_pos = 3;
+            *bin_data++ = (leftchar << 4) | (this_ch >> 2);
+            leftchar = this_ch & 0x3;
+            break;
+        case 3:
+            quad_pos = 0;
+            *bin_data++ = (leftchar << 6) | (this_ch);
+            leftchar = 0;
+            break;
+        }
     }
 
-    return vec_decoded;
+    if(quad_pos) {
+        if(quad_pos == 1) {
+            char tmpMsg[128]{};
+            snprintf(tmpMsg, sizeof(tmpMsg),
+                    "Invalid base64-encoded string: "
+                    "number of data characters (%zd) cannot be 1 more "
+                    "than a multiple of 4",
+                    (bin_data - bin_data_start) / 3 * 4 + 1);
+            error_message = tmpMsg;
+            goto error_end;
+        } else {
+            error_message = "Incorrect padding.";
+            goto error_end;
+        }
+        error_end:
+        delete[] bin_data;
+        throw std::runtime_error(error_message);
+    }
+
+    done:
+    length = bin_data - bin_data_start;
+    return bin_data_start;
 }
 
 int main(void) {
     try {
         std::string encoded_string = read_file("Texts/64test.txt");
-        BYTES decoded_bytes = b64_decode(encoded_string);
-        print_array(decoded_bytes);
-        std::cout << std::endl;
-        
+        size_t length = encoded_string.length();
+        uint8_t *decoded_bytes = b64_decode(encoded_string, length, false);
+        BYTES byte_vec(decoded_bytes, decoded_bytes + length);
+        print_array(byte_vec);
         return 0;
 
     } catch(const DecodeError &e) {
