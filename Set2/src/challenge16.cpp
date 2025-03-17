6d3c6ce550f1d138fa5835f7997517ba1c364659
pez <perr@arch.com>
2025-03-17 14:02:53 +0000

    Function to append user_string and postfix created. Minor change to challenge13.

--- Set2/src/challenge16.cpp
+++ Set2/src/challenge16.cpp
@@ -1,5 +1,7 @@
 #include "utility.hpp"
+#include <algorithm>
 #include <exception>
+#include <iterator>
 #include <string>
 
 class InputError : public std::exception {
@@ -30,12 +32,15 @@ class GetHacked {
             std::string cookie;
             std::string prefix = "comment1=cooking%20MCs;userdata=";
             std::string postfix = ";comment2=%20like%20a%20pound%20of%20bacon";
-
-
-
-            cookie.insert(cookie.begin(), prefix.begin(), prefix.end());
-            cookie.insert(cookie.end(), user_input.begin(), user_input.end());
-            cookie.insert(cookie.end(), postfix.begin(), postfix.end());
+            char forbidden[] = {';' ,'='};
+            
+            user_input.erase(std::remove_if(user_input.begin(), user_input.end(), [&](char c){
+                return std::find(std::begin(forbidden), std::end(forbidden), c) != std::end(forbidden);
+            }), user_input.end());
+            
+            cookie.append(prefix);
+            cookie.append(user_input);
+            cookie.append(postfix);
 
             return cookie;
         }
@@ -43,7 +48,7 @@ class GetHacked {
 
 int main() {
     GetHacked server;
-    std::string cookie = server.create_cookie("Hello World!");
+    std::string cookie = server.create_cookie("H==el==l;;o===;;;;;;;;;World!");
     
     std::cout << cookie << std::endl;
 
