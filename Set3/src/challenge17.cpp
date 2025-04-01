20d21efdec10b6b437438cc5aff0caa286770c7d
pez <perr@arch.com>
2025-04-01 18:38:07 +0100

    Challenge 17 started :')

--- Set3/src/challenge17.cpp
+++ Set3/src/challenge17.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "utility.hpp"
 
 int main(void) {
-    std::cout << "Hello World!" << std::endl;
+    std::map<int, std::vector<uint8_t>> texts = read_lines("Texts/challenge17.txt");
+
+    auto iter = texts.begin();
+
+    while (iter != texts.end()) {
+        print_array(iter->second); 
+        std::cout << '\n';
+        iter++;
+    }
+
     return 0;
 }
