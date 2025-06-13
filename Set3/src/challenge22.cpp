2a616e896688769da0a797d5eba5cd8684c30ad3
pez <perr@arch.com>
2025-06-13 16:57:26 +0100

    Crack MT19937 seed from output by guessing time PRNG was seeded.

--- Set3/src/challenge22.cpp
+++ Set3/src/challenge22.cpp
@@ -1,18 +1,53 @@
+#include <cassert>
+#include <cstdint>
 #include <random>
+#include <sys/types.h>
 #include <thread>
 #include <utility.hpp> 
 
-int main(void) {
+struct output {
+    uint32_t result;
+    std::time_t seed;
+};
+
+output get_output(void) {
+    output answer;
     std::random_device rd;
     std::cout << "Start Time: " << time(NULL) << '\n';
-    int wait_period = rd() % 5;
+    int wait_period = rd() % 100;
     std::cout << "Wait Time: " << wait_period << '\n';
 
     std::this_thread::sleep_for(std::chrono::seconds(wait_period));
-    std::cout << "Seed Time: " << time(NULL) << '\n';
+    std::time_t seed_time = time(NULL);
 
     cp::MT19937 rng(time(NULL));
-    std::cout << "RNG: " << rng()  << '\n';
+    
+    uint32_t random_number = rng();
+    answer.result = random_number;
+    answer.seed = seed_time;
+
+    return answer;
+}
+
+std::time_t guess_seed(uint32_t random_number) {
+    std::time_t now = std::time(nullptr);
+    for (std::time_t t = now - 100; t <= now; ++t) {
+        cp::MT19937 rng(t);
+        if (rng() == random_number) {
+            return t;
+        }
+    }
+
+    return 0;
+}
+
+int main(void) {
+    output random_number = get_output();
+
+    std::time_t seed = guess_seed(random_number.result);
+
+    assert(seed == random_number.seed);
+    std::cout << "Seed: " << seed << std::endl;
 
     return 0;
 }
