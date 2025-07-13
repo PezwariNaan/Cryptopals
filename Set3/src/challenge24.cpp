ac474e3f8e4f87aab7464b6d2311ab63fb7280cc
pez <perr@arch.com>
2025-07-13 12:31:03 +0100

    Challenge 24 Completed! Set 3 Done :)

--- Set3/src/challenge24.cpp
+++ Set3/src/challenge24.cpp
@@ -1,5 +1,5 @@
 #include "utility.hpp"
-#include <cstdint>
+#include <chrono>
 
 struct answer {
     std::vector<uint8_t> plaintext;
@@ -52,19 +52,43 @@ answer brute_force(std::vector<uint8_t> ciphertext) {
     return result;
 }
 
+uint32_t check_token(uint32_t token, time_t start, time_t end) {
+    for (time_t t = start; t <= end; t++) {
+        cp::MT19937 mt((uint32_t) t);
+        uint32_t candidate = mt();
+        
+        if (candidate == token) 
+            return t;
+    }
+
+    return 0;
+}
+
 int main(void) {
     std::string plaintext = "Hello World!";
     plaintext.append(16, 'A');
     std::vector<uint8_t> plainbytes(plaintext.begin(), plaintext.end());
 
-    uint16_t seed = time(NULL);
-    std::vector<uint8_t> ciphertext = mt19937_stream_cipher(seed, plainbytes);
+    time_t seed = time(NULL);
+    std::vector<uint8_t> ciphertext = mt19937_stream_cipher((uint16_t) seed, plainbytes);
 
+    // Part 1 - Recover 16bit seed.
     answer result = brute_force(ciphertext);
-
+    std:: cout << "Plaintext: ";
     print_array(result.plaintext);
-    std::cout << '\n' << result.seed << std::endl;
+    std::cout << "\nSeed: " << result.seed << "\n\n";
 
+    // Part 2 - Check if token is a product of MT19937 seeded with current time. 
+    cp::MT19937 mt(seed);
+    uint32_t reset_token = mt(); 
 
+    auto end = std::chrono::system_clock::now();
+    auto start = end - std::chrono::minutes(4);
+    time_t end_time = std::chrono::system_clock::to_time_t(end);
+    time_t start_time = std::chrono::system_clock::to_time_t(start);
+
+    uint32_t cracked = check_token(reset_token, start_time, end_time);
+
+    std::cout << "Token " << reset_token << " Seeded With: " << cracked << std::endl;
     return 0;
 }
